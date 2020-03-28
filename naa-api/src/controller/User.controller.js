b1a98525361fe07d7d632ef383241bf0d7d6c05a
Valentin <val.mon@orange.fr>
2020-03-28 18:11:51 +0100

    server working

--- naa-api/src/controller/User.controller.js
+++ naa-api/src/controller/User.controller.js
@@ -2,32 +2,28 @@ var User = require('../models/User.model');
 
 var mongoose = require('mongoose')
 
-class UserController{
-
- createUser (req,res,next){
-        console.log(req)
-         
-        var user = new User ({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-            pseudo: req.body.pseudo,
-            createdAt: Date.now()
-        });
-        user.save( (err,user)=>{
-            if(err){
-                return res.status(500).send(err)
-            }
-            else{
-                return res.status(200).json(user);
-            }
-        } )
-        
-    }
-
+ createUser = (req,res,next) =>{
+    console.log("Post Request"+req.body.firstName)
+     
+    var user = new User ({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        pseudo: req.body.pseudo,
+        createdAt: Date.now()
+    });
+    user.save( (err,user)=>{
+        if(err){
+            return res.status(500).send(err)
+        }
+        else{
+            console.log("User saved : "+user)
+            return res.status(200).json(user);
+        }
+    } )
+    
 }
 
 
-
-module.exports = UserController  ; 
+module.exports = {createUser}  ; 
