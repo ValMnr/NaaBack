0f4f60c9653ef7eef0b1af094b94324f66dd7ad3
Annelise999 <anne-liseherve@macbook-air-de-anne-lise.home>
2020-03-28 20:03:37 +0100

    requete profile humor

--- naa-api/src/controller/ProfileHumor.controller.js
+++ naa-api/src/controller/ProfileHumor.controller.js
@@ -1,5 +1,6 @@
 var ProfileHumorModel = require('../models/ProfileHumor.model');
 
+
 class ProfileHumor {
     createProfileHumor(req,res){
         if (!req.body.userId || !req.body.level || !req.body.causes || !req.body.sessionHumorDoneId) {
@@ -7,25 +8,48 @@ class ProfileHumor {
 
             
         }else {
-            var newProfileHumor = new ProfileHumor({
-                usedId: req.body.userId,
+            var newProfileHumor = new ProfileHumorModel({
+                userId: req.body.userId,
                 createdAt: Date.now(),
                 level: req.body.level,
                 causes: req.body.causes,
-                sessionHumorDoned : req.body.sessionHumorDoneId
+                sessionHumorDoneId : req.body.sessionHumorDoneId
             });
            
-            newProfileHumor.save((err) => {
+            newProfileHumor.save((err,newProfileHumor) => {
                 if (err) {
                     console.log(err);
                     return res.json({success: false});
                 }
+                else {
                 
-                res.json({success: true, msg: 'profile humeur créée avec succès'});
+                //res.json({success: true, msg: 'profil humeur créé avec succès'});
+                return res.status(200).json(newProfileHumor);
+            }
+                
                 
             });
         }
     }
+
+    //donne tous les profils humor d'un userId
+    async getProfileHumor(req, res) {
+        if (!req.query.userId) {
+            res.json({ success: false, msg: 'il faut l_id du user' });
+        } else {
+            try {
+                var humors = await ProfileHumorModel.find( { userId: req.query.userId }).lean().exec();
+                return res.status(200).json(humors);
+
+            }
+            catch (err) {
+                return res.json(err);
+            }
+
+        }
+    }
+
+
     
 }
 
