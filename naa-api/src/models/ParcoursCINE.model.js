e23263946fa4676bd24c744b523c81ff1ac21557
Annelise999 <anne-liseherve@macbook-air-de-anne-lise.home>
2020-04-03 15:13:18 +0200

    rectif done

--- naa-api/src/models/ParcoursCINE.model.js
+++ naa-api/src/models/ParcoursCINE.model.js
@@ -2,9 +2,11 @@
 var mongoose = require('mongoose');
 
 
+
 var ParcoursCINEModel = new mongoose.Schema({
     type: String, //C, I, N ou E
     rang: Number, //numéro du parcours : 1, 2, 3, 4 //
+    content: String, 
     selfEsteem: Number, //le score max pour ce parcours
     serenity: Number,
     confiance: Number,
