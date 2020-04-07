a2108bafed53f00d823870a02ef2a46cf6a76b82
Annelise999 <anne-liseherve@macbook-air-de-anne-lise.home>
2020-04-07 17:18:16 +0200

    Inscription Profile

--- naa-api/src/controller/Profile.controller.js
+++ naa-api/src/controller/Profile.controller.js
@@ -1,32 +1,43 @@
 var ProfileModel = require('../models/Profile.model');
+var User = require('../models/User.model')
 
 
 createProfile = (req,res,next)=>{
-    console.log(req.body)
-    var profile = new ProfileModel ({
-        userId: req.body.userId,
-        sexe: req.body.sexe,
-        age: req.body.age,
-        socialSituation: req.body.socialCat,
-        proffessionalSituation: req.body.proffesSituation,
-        children: req.body.children,
-        stressFactors: req.body.stressFactor,
-        stressEscape: req.body.stressEscape,
-        stressRelief: req.body.stressRelief,
-        emotionalConseq: req.body.emotionConseq,
-        physicalConseq: req.body.physicalConseq,
-        createdAt: Date.now()
-    })
-    profile.save((err,profile)=>{
-        if (err){return res.status(500).send(err) }
-        else{
-            console.log("User saved : " + profile)
-            return res.status(200).json(profile);
+
+    User.findOne({ email: req.body.email }, (err, result) => {
+        if (result != null) {
+            console.log(result);
+            var profile = new ProfileModel ({
+                userId: result._id,
+                sexe: req.body.sexe,
+                age: req.body.age,
+                socialSituation: req.body.socialSituation,
+                professionalSituation: req.body.professionalSituation,
+                children: req.body.children,
+                stressFactors: req.body.stressFactors,
+                stressEscape: req.body.stressEscape,
+                stressRelief: req.body.stressRelief,
+                emotionalConseq: req.body.emotionalConseq,
+                physicalConseq: req.body.physicalConseq,
+                createdAt: Date.now()
+
+
+
+            })
+            console.log(profile)
+            profile.save((err,profile)=>{
+                if (err){
+                    console.log(err) 
+                      }
+                else{
+                    console.log("Profile saved : " + profile)
+                    return res.status(200).json({profile: profile});
+                }
+            })  
+          
+        } else {
+            console.log(err);
         }
-    })
-
-
-
+    });  
 }
-
 module.exports = {createProfile};
